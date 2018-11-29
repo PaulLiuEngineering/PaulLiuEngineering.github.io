2f6c9e6f8bff2a1b18412ce8b79c446e3255e888
PaulLiuEngineering <paulliu14@gmail.com>
2018-11-29 14:53:28 -0600

    '3d'

--- me270/home.js
+++ me270/home.js
@@ -1,24 +1,38 @@
 var camera, scene, renderer;
 var geometry, material, mesh;
+var controls, objLoader;
 
 
 
 function init() {
-
-	camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
-	camera.position.z = 1;
-
 	scene = new THREE.Scene();
+	camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-	geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
-	material = new THREE.MeshNormalMaterial();
-
-	mesh = new THREE.Mesh( geometry, material );
-	scene.add( mesh );
-
-	renderer = new THREE.WebGLRenderer( { antialias: true } );
+	renderer = new THREE.WebGLRenderer();
 	renderer.setSize( window.innerWidth, window.innerHeight );
 	document.body.appendChild( renderer.domElement );
+
+	geometry = new THREE.BoxGeometry( 1, 1, 1);
+	material = new THREE.MeshBasicMaterial({color:0x00ff00});
+
+	camera.position.z = 200;
+
+	// controls = new THREE.OrbitControls(camera, renderer.domElement);
+	// controls.enableDamping = true;
+	// controls.campingFactor = 0.25;
+	// controls.enableZoom = true;
+
+	objLoader = new THREE.OBJLoader();
+	objLoader.load('new_speaker_mount.obj', function(object){
+		object.position.y -=60;
+		scene.add(object);
+	});
+	
+
+	
+	// scene.add( mesh );
+	
+	
 
 }
 
@@ -26,8 +40,8 @@ function animate() {
 
 	requestAnimationFrame( animate );
 
-	mesh.rotation.x += 0.01;
-	mesh.rotation.y += 0.02;
+	// mesh.rotation.x += 0.01;
+	// mesh.rotation.y += 0.02;
 
 	renderer.render( scene, camera );
 

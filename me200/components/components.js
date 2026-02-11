135f54ca61954289bf6b11d1ee922471f4ea32cf
PaulLiuEngineering <paulliu14@gmail.com>
2026-02-11 07:22:54 -0500

    Remove unused resources and legacy files

--- me200/components/components.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-    $('.map').maphilight(
-       
-    ).parent().css({"margin":"0 auto"});
-});
--- me200/cycles/cycles.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    
-    $('img').mapster({
-        fillColor: 'ff0000',
-        fillOpacity: 0.3,
-    }).parent().css({"margin":"0 auto"});
-   
-});
